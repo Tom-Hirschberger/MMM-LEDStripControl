bdef3821f3a16356746ac5e3a8966d8b900da369
Tom Hirschberger <git@hirschberger.eu>
2021-05-01 12:37:09 +0200

    modified the ESP32LedStripWithPongAndMqtt example to load settings with an external ".h" file; added an example ".h" file as well;

--- examples/ESP32LedStripWithPongAndMqtt/ESP32LedStripWithPongAndMqtt.ino
+++ examples/ESP32LedStripWithPongAndMqtt/ESP32LedStripWithPongAndMqtt.ino
@@ -16,33 +16,9 @@
 #include <FastLED.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
-
-#define MAX_LEDS 300 //All leds will be switched off. But only NUM_LEDS will be used.
-#define NUM_LEDS 300
-#define NUM_PONG_LEDS 10
-#define DATA_PIN 14
-#define BTN_1_GPIO 23
-#define BTN_2_GPIO 22
-#define BTN_DEBOUNCE_DELAY 500
-#define PONG_BTN_DELAY 2.000
-#define PONG_MAX_WINS 2
-#define PONG_TOLERANCE 2
-#define PONG_INIT_LED_DELAY 0.500
-#define PONG_DEC_PER_RUN 0.05
-#define PONG_MIN_LED_DELAY 0.02
-#define PONG_RESULT_DELAY_DURING 2.0
-#define PONG_RESULT_DELAY_AFTER 5.0
-#define REFRESH_INTERVAL 10000
+#include "settings.h"
 
 CRGB leds[MAX_LEDS];
-
-const char* SSID = "ENTER_WIFI_SSID_HERE";
-const char* PSK = "ENTER_WIFI_PASSWORD_HERE";
-const char* mqtt_broker = "ENTER_MQTT_SERVER_ADDRESS_HERE";
-const char* mqtt_user = "ENTER_MQTT_USERNAME_HERE";
-const char* mqtt_pass = "ENTER_MQTT_PASSWORD_HERE";
-const String client_name = "ESP_LED";
-const String topic_id = "esp_led";
  
 WiFiClient espClient;
 PubSubClient client(espClient);
